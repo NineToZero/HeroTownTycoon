428cad001978adba426913bc48932f673592c69a
homebd <118789343+homebd@users.noreply.github.com>
2023-12-14 18:08:07 +0900

    [Refactor] object pooling

--- Assets/Scripts/Items/Collectable.cs
+++ Assets/Scripts/Items/Collectable.cs
@@ -1,39 +1,24 @@
-using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
 public class Collectable : MonoBehaviour
 {
-    private SpriteRenderer spriteRenderer;
-    private Collider _collider;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Collider _collider;
+    private int _itemId;
 
-    private WaitForSeconds _secondsByTriggerEnable = new WaitForSeconds(0.5f);
-
-    public int ItemId;
-
-    private void Awake()
+    public void Init(int itemId, Vector3 position)
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        _collider = GetComponent<Collider>();
-    }
-
-    public void Init(int itemId)
-    {
-        ItemId = itemId;
+        _itemId = itemId;
         spriteRenderer.sprite = Managers.Instance.DataManager.GetItemSprites(itemId);
-        StartCoroutine(EnableTrigger());
-    }
+        transform.position = position;
+        gameObject.SetActive(true);
 
-    private IEnumerator EnableTrigger()
-    {
-        yield return _secondsByTriggerEnable;
-        _collider.enabled = true;
-    }
-
-    private void OnEnable()
-    {
         _collider.enabled = false;
+        Invoke(nameof(SetEnabled), 0.5f);
     }
+
+    private void SetEnabled() => _collider.enabled = true;
 
     private void OnTriggerEnter(Collider collision)
     {
@@ -41,8 +26,8 @@ public class Collectable : MonoBehaviour
 
         if (isValid)
         {
-            if (inventoryController.Inventory.Add(ItemId))
-                Destroy(this.gameObject); // TODO : 오브젝트 풀링으로!
+            if (inventoryController.Inventory.Add(_itemId))
+                gameObject.SetActive(false);
         }
     }
 }
--- Assets/Scripts/Managers/ItemManager.cs
+++ Assets/Scripts/Managers/ItemManager.cs
@@ -1,19 +1,59 @@
 using UnityEngine;
-using Random = UnityEngine.Random;
+using System.Collections.Generic;
 
 public class ItemManager : MonoBehaviour
-{ 
+{
+    private Queue<Collectable> _activeItems;
+    private Queue<Collectable> _deactiveItems;
+
+    private void Awake()
+    {
+        _activeItems = new();
+        _deactiveItems = new();
+    }
+
     public void SpawnCollectable(int itemId, Vector3 position, int count = 1)
     {
+        Collectable item;
+        float radius;
+        float angle;
+
+        if (_deactiveItems.Count < count)
+        {
+            int activeCount = _activeItems.Count;
+
+            for (int i = 0; i < activeCount; ++i)
+            {
+                item = _activeItems.Dequeue();
+
+                if (item.gameObject.activeSelf)
+                    _activeItems.Enqueue(item);
+                else
+                    _deactiveItems.Enqueue(item);
+            }
+
+            if (_deactiveItems.Count < count)
+            {
+                Collectable collectablePrefab = Managers.Instance.DataManager.GetPrefab<Collectable>(Const.Prefabs_Collectable);
+
+                for (int i = _deactiveItems.Count; i < count; ++i)
+                {
+                    _deactiveItems.Enqueue(Instantiate(collectablePrefab));
+                }
+            }
+        }
+
         for (int i = 0; i < count; ++i)
         {
-            float radius = Random.Range(0f, 0.5f);
-            float angle = Random.Range(0f, 2 * Mathf.PI);
-            position += new Vector3(radius * Mathf.Cos(angle), 0f, radius * Mathf.Sin(angle));
+            radius = Random.Range(0f, 0.5f);
+            angle = Random.Range(0f, 2 * Mathf.PI);
 
-            Collectable collectablePrefab = Managers.Instance.DataManager.GetPrefab<Collectable>(Const.Prefabs_Collectable);
-            Collectable collectable = Instantiate(collectablePrefab, position + 0.5f * Vector3.up, Quaternion.identity);
-            collectable.Init(itemId);
+            position += new Vector3(radius * Mathf.Cos(angle), 0, radius * Mathf.Sin(angle));
+            position.y = 0.5f;
+
+            item = _deactiveItems.Dequeue();
+            _activeItems.Enqueue(item);
+            item.Init(itemId, position);
         }
     }
 }
